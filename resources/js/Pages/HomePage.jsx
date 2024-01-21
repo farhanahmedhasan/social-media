31f4319656bc2c6eb1e0f04ed4b23441f6c5dc42
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2024-01-21 11:39:23 +0600

    added homepage controller

---
+++ resources/js/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import {Head} from '@inertiajs/react';
+
+export default function HomePage() {
+    return (
+        <>
+            <Head title="Socially" />
+            <p>Socially</p>
+        </>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\HomePageController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return Inertia::render('Home', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->middleware(['auth', 'verified'])->name("home");
+Route::get('/', [HomePageController::class, 'index'])->middleware(['auth', 'verified'])->name("homepage");
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
